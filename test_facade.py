a9577a9a38471ed89a6a14dcb7780f364cca392c
Yuuki <104145158+Clydewtf@users.noreply.github.com>
2023-12-17 23:18:21 +0700

    Add files via upload

--- test_facade.py
+++ test_facade.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from unittest.mock import patch, mock_open, call
 from facade import GameFacade, Level
 
@@ -21,6 +22,24 @@ class TestGameFacade(unittest.TestCase):
         self.game.save_high_score('test_player', 150)
         mock_file.assert_called_once_with('high_scores.txt', 'w', encoding='utf-8')
         mock_file().write.assert_called_once_with('test_player,150, test_level\n')
+
+    @patch('facade.os.listdir')  # заменяем функцию os.listdir на мок
+    @patch('facade.Level')  # заменяем класс Level на мок
+    def test_load_levels(self, mock_level, mock_listdir):
+        # Настраиваем моки
+        mock_listdir.return_value = ['level1.txt', 'level2.txt',
+                                     'not_a_level.pdf']  # os.listdir теперь вернет этот список файлов
+
+        game = GameFacade()  # создаем экземпляр класса, который будем тестировать
+
+        # Вызываем тестируемый метод
+        game.load_levels('levels')  # вызываем метод с аргументом 'levels'
+
+        # создаем список ожидаемых вызовов функции Level с правильными аргументами
+        calls = [call(os.path.join('levels', 'level1.txt')), call(os.path.join('levels', 'level2.txt'))]
+        mock_level.assert_has_calls(calls,
+                                    any_order=True)  # проверяем, что функция Level была вызвана с правильными аргументами
+        self.assertEqual(2, len(game.levels))  # проверяем, что в game.levels теперь два элемента
 
 if __name__ == '__main__':
     unittest.main()
